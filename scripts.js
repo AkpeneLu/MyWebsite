8cbd1c76fe44fb4e3a0645da2f581f257c9313d8
Francesca Akpene Lumor <francesca.lumor@gmail.com>
2020-08-21 20:55:16 +0200

    cleanup code, fixed anchor navigation

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
 window.onscroll = function () { stickyHeaderFunction() };
 
 var header = document.getElementById("myHeader");
-var nav = document.getElementsByClassName("nav-link");
+var nav = document.getElementsByClassName("nav");
 var social = document.getElementsByClassName("social-linkInHeader");
 var sticky = header.offsetTop;
 
 function stickyHeaderFunction() {
     if (window.pageYOffset > sticky) {
         header.classList.add("sticky");
-        nav.classList.add("sticky");
+        nav.classList.add("sticky"); // not working
         social.classList.add("sticky");
     } else {
         header.classList.remove("sticky");
-        nav.classList.remove("sticky");
+        nav.classList.remove("sticky"); // not working
         social.classList.remove("sticky");
     }
 }
