4413257bc462761d4a5b5a82a526daf58caeb1e8
Francesca Akpene Lumor <francesca.lumor@gmail.com>
2020-08-22 17:16:28 +0200

    fixed alignment in header++

--- scripts.js
+++ scripts.js
@@ -8,11 +8,11 @@ var sticky = header.offsetTop;
 function stickyHeaderFunction() {
     if (window.pageYOffset > sticky) {
         header.classList.add("sticky");
-        nav.classList.add("sticky"); // not working
-        social.classList.add("sticky");
+        //nav.classList.add("sticky");  not working
+        //social.classList.add("sticky");
     } else {
         header.classList.remove("sticky");
-        nav.classList.remove("sticky"); // not working
-        social.classList.remove("sticky");
+        //nav.classList.remove("sticky");  not working
+        //social.classList.remove("sticky");
     }
 }
